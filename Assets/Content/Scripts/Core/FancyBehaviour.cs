b9907ccbb3586289ed0acd7681605b79d3d6cc6b
_kote <logkedr18@gmail.com>
2026-05-19 18:18:30 +0400

    add: Abstract FancyBehaviour class for better event handling

---
+++ Assets/Content/Scripts/Core/FancyBehaviour.cs
@@ -0,0 +1,71 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public abstract class FancyBehaviour : MonoBehaviour
+{
+    [Inject]
+    private IEventSystem _eventSys;
+
+    private struct SubscribedEvent
+    {
+        public System.Delegate Callback;
+        public System.Type EventType;
+    }
+
+    private List<SubscribedEvent> _subscribedEvents = new List<SubscribedEvent>();
+
+    void OnEnable()
+    {
+        InitializeEvents();
+        OnScriptEnabled();
+    }
+
+    void OnDisable()
+    {
+        foreach (var sub in _subscribedEvents)
+        {
+            UnsubscribeEvent(sub.Callback, sub.EventType);
+        }
+
+        _subscribedEvents.Clear();
+
+        OnScriptDisabled();
+    }
+
+    protected virtual void OnScriptEnabled()
+    {
+    }
+
+    protected virtual void OnScriptDisabled()
+    {
+    }
+
+    protected virtual void InitializeEvents()
+    {
+    }
+
+    protected void RaiseEvent<T>(ref T eventData) where T : struct
+    {
+        _eventSys.RaiseEvent(ref eventData);
+    }
+
+    protected void SubscribeEvent<T>(EventCallback<T> callback) where T : struct
+    {
+        _eventSys.Subscribe(callback);
+        _subscribedEvents.Add(new SubscribedEvent { Callback = callback, EventType = typeof(T) });
+    }
+
+    private void UnsubscribeEvent(System.Delegate del, System.Type eventType)
+    {
+        if (del == null || eventType == null)
+            return;
+
+        var unsubscribeMethod = _eventSys.GetType().GetMethod("Unsubscribe");
+        if (unsubscribeMethod == null)
+            return;
+
+        var generic = unsubscribeMethod.MakeGenericMethod(eventType);
+        generic.Invoke(_eventSys, new object[] { del });
+    }
+}
